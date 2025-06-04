65329c2f1799fad99fccd40c235bcfea4234a7c9
Matías Vergara <vergaralopezmatias@gmail.com>
2025-06-04 11:52:08 -0400

    test: fix page specs imports

--- frontend/prepmate/src/app/pages/login/login.page.spec.ts
+++ frontend/prepmate/src/app/pages/login/login.page.spec.ts
@@ -1,11 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 import { LoginPage } from './login.page';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LoginPage],
+      imports: [IonicModule.forRoot(), ReactiveFormsModule, FormsModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
