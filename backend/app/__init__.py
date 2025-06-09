d6a6b732d2f19d5515c2135d0321d57e1c5a9f23
Matías Vergara <vergaralopezmatias@gmail.com>
2025-06-08 21:05:22 -0400

    feat: add CORS headers to responses

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -14,6 +14,13 @@ def create_app(config_override=None):
     if config_override:
         app.config.update(config_override)
 
+    @app.after_request
+    def add_cors_headers(response):
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+        response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
+        return response
+
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
