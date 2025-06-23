ba0da49725bb556c99e3e6ff949ff88272ce413c
Matías Vergara <vergaralopezmatias@gmail.com>
2025-06-23 13:31:32 -0400

    Extend JWT expiry

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,3 +8,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
